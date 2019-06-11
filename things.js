f5adb0893c45b84d053c5ebe090c6d4528520f5f
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-11 15:56:07 +0300

    node 4.1

--- things.js
+++ things.js
@@ -2,11 +2,16 @@ var array_counter = function(array) {
 	return "В массиве находяться " + array.length + " элемента";
 };
 var multiply = function(x, y) {
-	return (x+" умножить "+y+" равно"+ (x*y));
+	return (x+" умножить "+y+" равно"+ ( x*y));
 };
 var some_value = 451;
 module.exports.array_counter = array_counter;
 module.exports.multiply = multiply;
 module.exports.some_value = some_value;
 
+module.exports = {
+	array_counter: array_counter,
+	multiply: multiply,
+	some_value: some_value
+};
 
