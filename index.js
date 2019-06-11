716c396b482a44c1580b1a03adfcfd32e5756f03
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-11 16:53:52 +0300

    node 6.2

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var fs = require('fs');
 
 var file_readed=fs.readFileSync('text.txt', 'utf8');
 console.log(file_readed);
+var message = "Привет мир!\n" +file_readed;
+fs.writeFileSync('some_new_file.txt', message);
