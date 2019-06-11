2ff25d8f7276750ea730c0d06b5aebbc7bbb0669
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-11 16:44:16 +0300

    node 6.0

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-var events = require('events');
-var util = require('util');
-var myEmit = new events.EventEmitter();
-myEmit.on('some_event', function(text) {
-	console.log(text);
-});
-myEmit.emit('some_event', 'Обработчик собітий стаботал!');
-var Cars = function(model) {
-	this.model = model;
-};
-util.inherits(Cars, events.EventEmitter);
+var fs = require('fs');
 
-var bmw = new Cars('BMW');
-var audi = new Cars('Audi');
-var volvo = new Cars('Volvo');
-
-var cars = [bmw, audi, volvo];
-cars.forEach(function(car){
-	car.on(' speed', function(text) {
-		console.log(car.model+"speed  is -" + text);
-	});
-});
-
-bmw.emit('speed', '254.5 km');
-volvo.emit('speed', '243.5 km');
-audi.emit('speed', '233.3 km');
+var file_readed=fs.readFileSync('text.txt', 'utf8');
+console.log(file_readed);
