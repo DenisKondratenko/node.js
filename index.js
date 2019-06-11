580cb1288d5e427f5c9e45ee5f006263db4bc08a
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-11 17:21:39 +0300

    node 6.4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var fs = require('fs');
 
-var file_readed=fs.readFile('text.txt', 'utf8', function(err, data) {
+fs.readFile('text.txt', 'utf8', function(err, data) {
 	console.log(data);
 });
+fs.writeFile('some.txt', 'Hi, it is me', function(err, data) {});
+
 
 console.log("Test");
