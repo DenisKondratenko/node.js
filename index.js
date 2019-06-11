af59c7fe99feb98f6f2ec59ccbf0cf3fb8d5b14e
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-11 13:08:46 +0300

    node leson1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log ("hello world");
+
+var s=0;
+if (s == 0)
+{
+	console.log("work");
+}
+for(var s=0; s<5; s++)
+{
+	console.log(s);
+}
+console.log ("s = "+s);
+console.log(__filename);
+console.log(__dirname);
