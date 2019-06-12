f21174bb0fe744144f84c3f02fafbe75d550b90f
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-12 10:23:08 +0300

    node 7.1

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var fs = require('fs');
 //fs.unlinlink('file.txt', function(){console.log("указаный файл удален");}); 
-//fs.rmdir('new-one', function() {"указаная папка удалена"}); 
-fs.mkdir('new-one', function() {
-	fs.writeFile('./new-one/some_new.txt', 'Привет мир!', function() {
-		console.log("Все сраблтало!");
-	});
-});
+
+//fs.rmdir('new-one', function() {console.log("указаная папка удалена");}); 
+
+fs.unlink('./new-one/some_new.txt', function() {
+	console.log("указаный файл удален");
+	fs.rmdir('new-one', function() {
+		console.log("указаная папка удалена");
+	})
+}); 
+
+//fs.mkdir('new-one', function() {
+//	fs.writeFile('./new-one/some_new.txt', 'Привет мир!', function() {
+//		console.log("Все сраблтало!");
+//	});
+//});
