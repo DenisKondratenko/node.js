3a6f2cf5dcd526f2c41a70ea1cd5e792b5e8db33
DenisKondratenko <homiekondratenko@gmail.com>
2019-06-12 11:17:07 +0300

    node 9.1

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // потоки на node.js
 var fs = require('fs');
-var myReadShort = fs.createReadStream(__dirname + '/text.txt' 'utf8');
+
+//чтение потоками 
+//var myReadShort = fs.createReadStream(__dirname + '/text.txt', 'utf8');
+//myReadShort.on('data', function(chunk){
+//	console.log("Данные получены:\n" + chunk);
+//})
+
+var myReadShort = fs.createReadStream(__dirname + '/text.txt', 'utf8');
+var myWriteShort = fs.createWriteStream(__dirname + '/news.txt');
 myReadShort.on('data', function(chunk){
 	console.log("Данные получены:\n" + chunk);
+	myWriteShort.write(chunk);
 })
