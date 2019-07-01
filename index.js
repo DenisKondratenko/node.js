90a3ac7c33f1cad408e3018ca1e5d2ce0562bc8c
DenisKondratenko <homiekondratenko@gmail.com>
2019-07-01 10:36:44 +0300

    node 13.1

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express();
 
-app.listen(3000);
 
-app.get('', function(req, res){
+app.get('/', function(req, res){
 	res.send('this is home');
 }); 
+app.get('/test', function(req, res){
+	res.send('This is news');
+}); 
+app.get('/test/id', function(req, res){
+	res.send('ID is = ' + req.params.id);
+}); 
+app.listen(3000);
