f0762cbda50c43bc78b3266ecff9c71e0441ef7c
DenisKondratenko <homiekondratenko@gmail.com>
2019-07-01 10:23:25 +0300

    node 13.1

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-// потоки на node.js
-var fs = require('fs');
-// server на node.js 
-var http = require('http');
+var express = require('express');
 
-//fs.rmdir('new-one', function() {console.log("указаная папка удалена");}); 
-var server = http.createServer(function(req, res) {
-	console.log("URL страницы: " + req.url);
+var app = express();
 
-	//res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'}); вывод текста
-	//var myReadShort = fs.createReadStream(__dirname + '/text.txt', 'utf8');
+app.listen(3000);
 
-	//res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'}); вывод html
-	//var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
-
-    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-	var obj = {
-		model: 'Audi',
-		speed: '234.5',
-		wheels: 4
-	};
-
-res.end(JSON.stringify(obj));
-//myReadShort.pipe(res);
-	
-});
-
-
-
-server.listen(3000, '127.0.0.1');
-console.log ("Мы отслеживаем порт 3000"); 
+app.get('', function(req, res){
+	res.send('this is home');
+}); 
